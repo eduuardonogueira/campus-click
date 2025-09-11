c2c0dc16719edfd069a1302718992c6f7d1aec96
eduardo macOS <eduardocastroemp@gmail.com>
2025-09-11 19:11:58 -0300

    deleted some trash

--- backend/src/modules/room/room.controller.ts
+++ backend/src/modules/room/room.controller.ts
@@ -6,13 +6,11 @@ import {
   Patch,
   Param,
   Delete,
-  UsePipes,
   ParseIntPipe,
 } from '@nestjs/common';
 import { RoomService } from './room.service';
 import type { CreateRoomDto } from './dto/create-room.dto';
 import type { UpdateRoomDto } from './dto/update-room.dto';
-import { ValidationPipe } from '@nestjs/common';
 
 @Controller('room')
 export class RoomController {
@@ -30,8 +28,7 @@ export class RoomController {
 
   @Get(':id')
   findOne(@Param('id', ParseIntPipe) id: number) {
-    console.log(typeof id === 'number'); // true
-    return this.roomService.findOne(+id);
+    return this.roomService.findOne(id);
   }
 
   @Patch(':id')
@@ -39,13 +36,11 @@ export class RoomController {
     @Param('id', ParseIntPipe) id: number,
     @Body() updateRoomDto: UpdateRoomDto,
   ) {
-    console.log(typeof id === 'number'); // true
-    return this.roomService.update(+id, updateRoomDto);
+    return this.roomService.update(id, updateRoomDto);
   }
 
   @Delete(':id')
   remove(@Param('id', ParseIntPipe) id: number) {
-    console.log(typeof id === 'number'); // true
-    return this.roomService.remove(+id);
+    return this.roomService.remove(id);
   }
 }
