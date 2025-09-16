121d597c341cbf45b128359eb4cf556846b31cd6
GabrielInada <gabrieljunichiro2@gmail.com>
2025-09-16 16:20:56 -0300

    feat: CRUD amenities
    CRUD de amenities funcionando

--- backend/src/modules/amenities/amenities.service.ts
+++ backend/src/modules/amenities/amenities.service.ts
@@ -1,26 +1,51 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { CreateAmenityDto } from './dto/create-amenity.dto';
 import { UpdateAmenityDto } from './dto/update-amenity.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Amenity } from './entities/amenity.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class AmenitiesService {
-  create(createAmenityDto: CreateAmenityDto) {
-    return 'This action adds a new amenity';
+  constructor(
+    @InjectRepository(Amenity)
+    private amenitiesRepository: Repository<Amenity>,
+  ) {}
+
+  async create(createAmenityDto: CreateAmenityDto) {
+    const amenity = this.amenitiesRepository.create({ ...createAmenityDto });
+    return await this.amenitiesRepository.save(amenity);
   }
 
-  findAll() {
-    return `This action returns all amenities`;
+  async remove(id: number) {
+    const foundAmenity = await this.findOne(id);
+    return this.amenitiesRepository.delete(foundAmenity.id);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} amenity`;
+  async findAll() {
+    const findedAmenities = await this.amenitiesRepository.find();
+    if (!findedAmenities) {
+      throw new HttpException(`Amenities Not Found`, 404);
+    }
+    return findedAmenities;
   }
 
-  update(id: number, updateAmenityDto: UpdateAmenityDto) {
-    return `This action updates a #${id} amenity`;
+  async findOne(id: number) {
+    const amenity = await this.amenitiesRepository.findOne({ where: { id } });
+    if (!amenity) {
+      throw new HttpException(`Amenity with ID ${id} not found`, 404);
+    }
+    return amenity;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} amenity`;
+  async update(id: number, updateAmenityDto: UpdateAmenityDto) {
+    const amenity = await this.amenitiesRepository.preload({
+      id,
+      ...updateAmenityDto,
+    });
+    if (!amenity) {
+      throw new HttpException(`Amenity with ID ${id} not found`, 404);
+    }
+    return this.amenitiesRepository.save(amenity);
   }
 }
