0e78011ff1ddf7e2c377ee5289f36499946589c5
GabrielInada <gabrieljunichiro2@gmail.com>
2025-09-17 20:09:51 -0300

    Add health check endpoint to app controller
    um novo GET endpoint no AppController para verificar se o backend está ativo. vou usar no fetch() na task do Cron

--- backend/src/modules/app/app.controller.ts
+++ backend/src/modules/app/app.controller.ts
@@ -4,4 +4,10 @@ import { AppService } from './app.service';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  // Um endpoint para verificar se o backend está ativo (usado pelo self-consult)
+  @Get()
+  healthCheck() {
+    return this.appService.getHealthStatus();
+  }
 }
