002fc96e21745ca2fb74245fb23f513d187e29d7
GabrielInada <gabrieljunichiro2@gmail.com>
2025-09-17 20:12:23 -0300

    Update no configuration
    Pra manter o padrão de chamar as variáveis de ambiente pelo configuration.ts

--- backend/src/config/configuration.ts
+++ backend/src/config/configuration.ts
@@ -7,6 +7,9 @@ export default () => ({
   dbName: process.env.DB_NAME,
   dbUser: process.env.DB_USER,
   dbPass: process.env.DB_PASS,
+  externalUrl: process.env.RENDER_EXTERNAL_URL,
+  internalHostname: process.env.RENDER_INTERNAL_HOSTNAME,
+  hostUrl: process.env.HOST_URL,
 });
 
 export interface ValidationPipeOptions extends ValidatorOptions {
